a675e5657f6da61f0be6e010700b67477f6bf076
Marcelo Humberto Leiva Salazar <chelo.salazar017@gmail.com>
2019-10-04 16:05:50 -0600

    Deeterminación de la función

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let btnOn = 0;
 let btnOff = 0;
-let state = false;
+let state = true;
 
 var client = mqtt.connect('wss://polloLSW:PolloSecreto@broker.shiftr.io',{
   clientId:'Controlador Web'
@@ -20,6 +20,7 @@ var client = mqtt.connect('wss://polloLSW:PolloSecreto@broker.shiftr.io',{
      sp.style.border = "3px solid #fff";
      sp.classList.remove('sp');
      sp.classList.add('spa');
+     LedOn();
      state = false;
    } else {
      body.style.background = "#222";
@@ -28,6 +29,7 @@ var client = mqtt.connect('wss://polloLSW:PolloSecreto@broker.shiftr.io',{
      sp.style.border = "3px solid #000";
      sp.classList.remove('spa');
      sp.classList.add('sp');
+     LedOff();
      state = true;
    }
  }
@@ -36,7 +38,7 @@ var client = mqtt.connect('wss://polloLSW:PolloSecreto@broker.shiftr.io',{
    client.publish('/ALSW/Led','1');
  }
  function LedOff() {
-   console.log("Encender Led");
+   console.log("Apagar Led");
    client.publish('/ALSW/Led','0');
  }
 client.on('connect',function(){
